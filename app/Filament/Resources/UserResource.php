b7f2c0c336215fbc4e3293fce4d04c6114ef4287
Yuefii <muffidahmadiawan31@gmail.com>
2024-12-06 17:15:55 +0700

    chore: adding management users dashboard services

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+    protected static ?string $label = 'Data Anggota';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->minLength(3),
+                TextInput::make('npm')->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->revealable()
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (string $context): bool => $context === 'create')
+                    ->minLength(6),
+                Select::make('role')->options([
+                    'admin' => 'Admin',
+                    'anggota' => 'Anggota',
+                    'bendahara' => 'Bendahara',
+                ])->required()->default('anggota'),
+                Toggle::make('is_active')->default(false),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nama Anggota')
+                    ->searchable(),
+                TextColumn::make('npm')
+                    ->label('NPM')
+                    ->searchable(),
+                TextColumn::make('role')
+                    ->searchable(),
+                IconColumn::make('is_active')
+                    ->label('Status')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d F Y')
+                    ->searchable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ])->label('Hapus Anggota'),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
