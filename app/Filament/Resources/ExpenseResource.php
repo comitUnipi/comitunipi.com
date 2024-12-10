436e8b1050b5583d11c627dc47a84722eaf8729f
Yuefii <muffidahmadiawan31@gmail.com>
2024-12-10 11:10:53 +0700

    chore: create data expense on dashboard

---
+++ app/Filament/Resources/ExpenseResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExpenseResource\Pages;
+use App\Filament\Resources\ExpenseResource\RelationManagers;
+use App\Models\Expense;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExpenseResource extends Resource
+{
+    protected static ?string $model = Expense::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Manajemen Keuangan';
+    protected static ?string $label = 'Data Pengeluaran';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('amount')
+                    ->label('Jumlah Pengeluaran')
+                    ->numeric()
+                    ->required(),
+                DatePicker::make('date')
+                    ->label('Tanggal Pengeluaran')
+                    ->default(now())
+                    ->required(),
+                TextInput::make('description')
+                    ->label('Keterangan')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('date')
+                    ->label('Tanggal Pengeluaran')
+                    ->dateTime('d F Y'),
+                TextColumn::make('amount')
+                    ->label('Jumlah Pengeluaran')
+                    ->money('IDR'),
+                TextColumn::make('description')
+                    ->limit(20)
+                    ->label('Keterangan'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExpenses::route('/'),
+            'create' => Pages\CreateExpense::route('/create'),
+            'edit' => Pages\EditExpense::route('/{record}/edit'),
+        ];
+    }
+}
