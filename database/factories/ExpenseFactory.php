bfdf405d8e0713468fbe42795484161efcd0c094
Yuefii <muffidahmadiawan31@gmail.com>
2024-12-13 09:08:29 +0700

    chore: create database seeder & factory for expense & refac kas factory

--- database/factories/ExpenseFactory.php
+++ database/factories/ExpenseFactory.php
@@ -2,30 +2,27 @@
 
 namespace Database\Factories;
 
-use App\Models\Activity;
-use App\Models\Kas;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Kas>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Expense>
  */
-class KasFactory extends Factory
+class ExpenseFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    protected $model = Kas::class;
-    
     public function definition(): array
     {
         return [
-            'user_id' => User::factory(),
-            'activity_id' => Activity::inRandomOrder()->first()->id,
-            'amount' => $this->faker->boolean(80) ? 5000 : 0,
+            'amount' => $this->faker->randomFloat(50000, 25000, 10000),
             'date' => $this->faker->dateTimeBetween('2024-10-06', '2024-12-15'),
+            'description' => $this->faker->sentence(),
+            'img_url' => $this->faker->imageUrl(),
+            'user_id' => User::where('role', 'bendahara')->inRandomOrder()->first()->id,
         ];
     }
 }
