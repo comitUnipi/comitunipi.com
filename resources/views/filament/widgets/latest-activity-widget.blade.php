8ed42a4476b828fe57671dab44976553d7cec82a
Yuefii <muffidahmadiawan31@gmail.com>
2024-12-13 14:11:14 +0700

    chore: refact widget latest activity

--- resources/views/filament/widgets/latest-activity-widget.blade.php
+++ resources/views/filament/widgets/latest-activity-widget.blade.php
@@ -1,7 +1,14 @@
 <x-filament-widgets::widget>
         <div class="w-full h-full">
-                <h3 class="text-base mb-1">Kegiatan Terbaru</h3>
-                <p class="text-2xl font-semibold mb-1">{{ $activityInfo }}</p>
-                <p class="text-sm text-blue-600">{{ $activity_date }} {{ $activity_location }}</p>
+                <div>
+                        <h3 class="text-base mb-1">Kegiatan Terbaru</h3>
+                        <p class="text-2xl font-semibold mb-1">{{ $activityInfo }}</p>
+                        <p class="text-sm text-blue-600">{{ $activity_date }} {{ $activity_location }}</p>
+                </div>
+                <div class="mt-10">
+                        <h3 class="text-base mb-1">Event Terbaru</h3>
+                        <p class="text-2xl font-semibold mb-1">Belum ada event</p>
+                        <!-- <p class="text-sm text-blue-600">{{ $activity_date }} {{ $activity_location }}</p> -->
+                </div>
         </div>
 </x-filament-widgets::widget>
