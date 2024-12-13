4384c12d98c8eda93c4141cd2b0911c14d6947c3
Yuefii <muffidahmadiawan31@gmail.com>
2024-12-13 13:26:58 +0700

    chore: create migration & dashboard monthly report

---
+++ database/migrations/2024_12_13_123204_create_monthly_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('monthly_reports', function (Blueprint $table) {
+            $table->id();
+            $table->date('report_date');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->decimal('total_kas', 15, 2)->default(0);
+            $table->decimal('total_income', 15, 2)->default(0);
+            $table->decimal('total_expense', 15, 2)->default(0);
+            $table->decimal('remaining_balance', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('monthly_reports');
+    }
+};
