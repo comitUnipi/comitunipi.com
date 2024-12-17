346261c5c7d7648bbdfb84e74a0ee6838643b754
Yuefii <muffidahmadiawan31@gmail.com>
2024-12-17 13:14:48 +0700

    chore: create protection policy for kas

--- app/Filament/Resources/KasResource/Pages/ListKas.php
+++ app/Filament/Resources/KasResource/Pages/ListKas.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\KasResource\Pages;
 
 use App\Filament\Resources\KasResource;
+use App\Models\Kas;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListKas extends ListRecords
 {
@@ -16,5 +18,17 @@ class ListKas extends ListRecords
             Actions\CreateAction::make()
                 ->label('Tambah Kas'),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        $user = auth()->user();
+        $query = Kas::query();
+        
+        if ($user->role === 'anggota') {
+            $query->where('user_id', $user->id);
+        }
+
+        return $query;
     }
 }
