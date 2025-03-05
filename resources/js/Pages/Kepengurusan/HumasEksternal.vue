c33c8a76f691b2cefa5a3dafadcc34de22875d40
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-05 21:49:23 +0700

    chore: styling components sdm, humas internal & eksternal

--- resources/js/Pages/Kepengurusan/HumasEksternal.vue
+++ resources/js/Pages/Kepengurusan/HumasEksternal.vue
@@ -1,3 +1,72 @@
 <template>
-  <div>Humas Eksternal</div>
+
+  <Head title="Humas Eksternal - Kepengurusan" />
+  <Main>
+    <BannerKepengurusan title="Humas Eksternal Comit" />
+    <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20">
+      <div class="container mx-auto">
+        <div class="flex flex-wrap gap-20 justify-center mx-4">
+          <div v-for="pengurus in kepengurusan" :key="pengurus.id" class="w-full sm:w-1/2 lg:w-1/3 px-4">
+            <div class="mb-10 flex flex-col items-center">
+              <div class="relative rounded-full z-10 mx-auto mb-6">
+                <img :src="pengurus.img" alt="image" class="w-[280px] h-[280px] object-cover rounded-full" />
+              </div>
+              <div class="text-center">
+                <h4 class="font-medium text-2xl text-dark mb-2">
+                  {{ pengurus.nama }}
+                </h4>
+                <p class="font-medium text-md text-body-color mb-5">
+                  {{ pengurus.job }}
+                </p>
+                <div class="flex items-center justify-center">
+                  <!-- icon -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Main>
 </template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import BannerKepengurusan from '@/components/BannerKepengurusan.vue';
+import Main from '@/Layouts/Main.vue';
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      kepengurusan: [
+        {
+          id: 1,
+          img: "/images/kepengurusan-2024-2025/humas-eksternal-1.jpg",
+          nama: "Azzrah Zerlinda Putri",
+          job: "Humas Eksternal",
+        },
+        {
+          id: 2,
+          img: "/images/kepengurusan-2024-2025/humas-eksternal-2.jpg",
+          nama: "Kadek Budayana",
+          job: "Humas Eksternal",
+        },
+        {
+          id: 3,
+          img: "/images/kepengurusan-2024-2025/humas-eksternal-3.jpg",
+          nama: "Nurfail Jazali",
+          job: "Humas Eksternal",
+        },
+        {
+          id: 4,
+          img: "/images/kepengurusan-2024-2025/humas-eksternal-4.jpg",
+          nama: "M. Teuku Rizal",
+          job: "Humas Eksternal",
+        },
+      ]
+    };
+  }
+}
+</script>
--- resources/js/Pages/Kepengurusan/Sdm.vue
+++ resources/js/Pages/Kepengurusan/Sdm.vue
@@ -1,3 +1,72 @@
 <template>
-  <div>Sdm</div>
+
+  <Head title="SDM - Kepengurusan" />
+  <Main>
+    <BannerKepengurusan title="SDM Comit" />
+    <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20">
+      <div class="container mx-auto">
+        <div class="flex flex-wrap gap-20 justify-center mx-4">
+          <div v-for="pengurus in kepengurusan" :key="pengurus.id" class="w-full sm:w-1/2 lg:w-1/3 px-4">
+            <div class="mb-10 flex flex-col items-center">
+              <div class="relative rounded-full z-10 mx-auto mb-6">
+                <img :src="pengurus.img" alt="image" class="w-[280px] h-[280px] object-cover rounded-full" />
+              </div>
+              <div class="text-center">
+                <h4 class="font-medium text-2xl text-dark mb-2">
+                  {{ pengurus.nama }}
+                </h4>
+                <p class="font-medium text-md text-body-color mb-5">
+                  {{ pengurus.job }}
+                </p>
+                <div class="flex items-center justify-center">
+                  <!-- icon -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Main>
 </template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import BannerKepengurusan from '@/components/BannerKepengurusan.vue';
+import Main from '@/Layouts/Main.vue';
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      kepengurusan: [
+        {
+          id: 1,
+          img: "/images/kepengurusan-2024-2025/sdm-1.jpg",
+          nama: "Maulana Laksana Putra",
+          job: "SDM",
+        },
+        {
+          id: 2,
+          img: "/images/kepengurusan-2024-2025/sdm-2.jpg",
+          nama: "Cindy Febry Yani",
+          job: "SDM",
+        },
+        {
+          id: 3,
+          img: "/images/kepengurusan-2024-2025/sdm-3.jpg",
+          nama: "Komalasari",
+          job: "SDM",
+        },
+        {
+          id: 4,
+          img: "/images/kepengurusan-2024-2025/sdm-4.jpg",
+          nama: "Cahaya Dewi P",
+          job: "SDM",
+        },
+      ]
+    };
+  }
+}
+</script>
