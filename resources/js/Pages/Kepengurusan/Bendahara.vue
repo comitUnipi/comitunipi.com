8a5e9e3b0d18037d42f2b620ffbd0225cb281b39
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-05 21:13:22 +0700

    chore: refact types on kepengurusan

--- resources/js/Pages/Kepengurusan/Bendahara.vue
+++ resources/js/Pages/Kepengurusan/Bendahara.vue
@@ -6,17 +6,17 @@
     <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20">
       <div class="container mx-auto">
         <div class="flex flex-wrap gap-20 justify-center mx-4">
-          <div v-for="mentor in mentors" :key="mentor.id" class="w-full sm:w-1/2 lg:w-1/4 px-4">
+          <div v-for="pengurus in kepengurusan" :key="pengurus.id" class="w-full sm:w-1/2 lg:w-1/4 px-4">
             <div class="mb-10 flex flex-col items-center">
               <div class="relative rounded-full z-10 mx-auto mb-6">
-                <img :src="mentor.img" alt="image" class="w-[280px] h-[280px] object-cover rounded-full" />
+                <img :src="pengurus.img" alt="image" class="w-[280px] h-[280px] object-cover rounded-full" />
               </div>
               <div class="text-center">
                 <h4 class="font-medium text-2xl text-dark mb-2">
-                  {{ mentor.nama }}
+                  {{ pengurus.nama }}
                 </h4>
                 <p class="font-medium text-md text-body-color mb-5">
-                  {{ mentor.job }}
+                  {{ pengurus.job }}
                 </p>
                 <div class="flex items-center justify-center">
                   <!-- icon -->
@@ -40,7 +40,7 @@ import Main from '@/Layouts/Main.vue';
 export default {
   data() {
     return {
-      mentors: [
+      kepengurusan: [
         {
           id: 1,
           img: "/images/kepengurusan-2024-2025/bendahara-1.jpg",
