34d9384cf5f1aac66ac3fe9787443081447d94ec
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-05 18:32:32 +0700

    chore: refact styling on footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,136 +1,117 @@
 <template>
   <footer class="bg-blue-800 pt-20 lg:pt-[120px] relative z-10">
-      <div class="container">
-        <div class="flex flex-wrap mx-5">
-          <div class="w-full sm:w-1/2 md:w-1/2 lg:w-4/12 xl:w-3/12 px-4">
+    <div class="container mx-auto">
+      <div class="flex flex-wrap mx-5">
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-4/12 xl:w-3/12 px-4">
+          <div class="w-full mb-10">
             <div class="w-full mb-10">
-              <a class="inline-block max-w-[160px] mb-6">
-                <img
-                  src="/images/logo_white.png"
-                  alt="logo"
-                  class="max-w-full"
-                />
-              </a>
-              <p class="text-base text-[#f3f4fe] mb-7">
-                Pelajari lebih dalam tentang ilmu teknologi dan komputer bersama kami
-              </p>
-            </div>
-          </div>
-          <div class="w-full sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12 px-4">
-            <div class="w-full mb-10">
-              <h4 class="text-white text-lg font-semibold mb-9">Tentang Kami</h4>
-              <ul>
+              <ul class="flex gap-x-5 items-center">
                 <li>
-                  <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Visi dan Misi</a>
+                  <a class="inline-block max-w-[160px] mb-6">
+                    <img src="/images/logo_white.png" alt="logo" class="max-w-full" />
+                  </a>
                 </li>
                 <li>
-                  <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Kepengurusan</a>
-                </li>
-                <li>
-                  <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Kegiatan</a>
+                  <a href="/" target="_blank" class="block mb-6 mr-5 max-w-[120px] xl:max-w-[100px] 2xl:max-w-[120px]">
+                    <img src="/images/unipi.png" alt="unipi" />
+                  </a>
                 </li>
               </ul>
             </div>
+            <p class="text-base text-[#f3f4fe] mb-7">
+              Pelajari lebih dalam tentang ilmu teknologi dan komputer bersama kami
+            </p>
           </div>
-          <div class="w-full sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12 px-4">
-            <div class="w-full mb-10">
-              <h4 class="text-white text-lg font-semibold mb-9">Features</h4>
-              <ul>
-                <li>
-                  <a
-                    class="inline-block
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12 px-4">
+          <div class="w-full mb-10">
+            <h4 class="text-white text-lg font-semibold mb-9">Tentang Kami</h4>
+            <ul>
+              <li>
+                <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Visi dan Misi</a>
+              </li>
+              <li>
+                <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Kepengurusan</a>
+              </li>
+              <li>
+                <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Kegiatan</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12 px-4">
+          <div class="w-full mb-10">
+            <h4 class="text-white text-lg font-semibold mb-9">Features</h4>
+            <ul>
+              <li>
+                <a class="inline-block
                       text-base text-[#f3f4fe]
                       hover:text-primary
                       leading-loose
                       mb-2
-                    "
-                  >
-                    Privacy policy
-                  </a>
-                </li>
-                <li>
-                  <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">
-                    Terms of Service
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="w-full sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12 px-4">
-            <div class="w-full mb-10">
-              <h4 class="text-white text-lg font-semibold mb-9">
-                Events
-              </h4>
-              <ul>
-                <li>
-                  <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">
-                    Workshop Data Analyst
-                  </a>
-                </li>
-                <li>
-                  <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Workshop Arduino</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="w-full md:w-2/3 lg:w-6/12 xl:w-3/12 px-4">
-            <div class="w-full mb-10">
-              <ul class="flex flex-wrap items-center">
-                <li>
-                  <a
-                    href="/"
-                    target="_blank"
-                    class="block mb-6 mr-5 max-w-[120px] xl:max-w-[100px] 2xl:max-w-[120px]"
-                  >
-                    <img
-                      src="/images/unipi.png"
-                      alt="comit"
-                    />
-                  </a>
-                </li>
-              </ul>
-            </div>
+                    ">
+                  Privacy policy
+                </a>
+              </li>
+              <li>
+                <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">
+                  Terms of Service
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12 px-4">
+          <div class="w-full mb-10">
+            <h4 class="text-white text-lg font-semibold mb-9">
+              Events
+            </h4>
+            <ul>
+              <li>
+                <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">
+                  Workshop Data Analyst
+                </a>
+              </li>
+              <li>
+                <a class="inline-block text-base text-[#f3f4fe] hover:text-primary leading-loose mb-2">Workshop
+                  Arduino</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="w-full md:w-2/3 lg:w-6/12 xl:w-3/12 px-4">
 
-      <div class="border-t border-opacity-40 py-8 mt-12 lg:mt-[60px]">
-        <div class="container">
-          <div class="flex flex-wrap mx-4">
-            <div class="w-full md:w-2/3 lg:w-1/2 px-4">
-              <div class="my-1">
-                <div
-                  class="flex items-center justify-center md:justify-start mx-3"
-                >
-                  <a
-                    class="text-base text-[#f3f4fe] hover:text-primary px-3"
-                  >
-                    Privacy policy
-                  </a>
-                  <a
-                    class="text-base text-[#f3f4fe] hover:text-primary px-3"
-                  >
-                    Terms of service
-                  </a>
-                </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="border-t border-opacity-40 py-8 mt-12 lg:mt-[60px]">
+      <div class="container">
+        <div class="flex flex-wrap mx-4">
+          <div class="w-full md:w-2/3 lg:w-1/2 px-4">
+            <div class="my-1">
+              <div class="flex items-center justify-center md:justify-start mx-3">
+                <a class="text-base text-[#f3f4fe] hover:text-primary px-3">
+                  Privacy policy
+                </a>
+                <a class="text-base text-[#f3f4fe] hover:text-primary px-3">
+                  Terms of service
+                </a>
               </div>
             </div>
-            <div class="w-full md:w-1/3 lg:w-1/2 px-4">
-              <div class="flex justify-center md:justify-end my-1">
-                <p class="text-base text-[#f3f4fe]">
-                  Copyright 2024 -
-                  <a
-                    href=""
-                    target="_blank"
-                    class="text-primary hover:underline"
-                  >
-                    Comunnity of Information Technology
-                  </a>
-                </p>
-              </div>
+          </div>
+          <div class="w-full md:w-1/3 lg:w-1/2 px-4">
+            <div class="flex justify-center md:justify-end my-1">
+              <p class="text-base text-[#f3f4fe]">
+                Copyright 2024 -
+                <a href="" target="_blank" class="text-primary hover:underline">
+                  Comunnity of Information Technology
+                </a>
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </footer>
+    </div>
+  </footer>
 </template>
