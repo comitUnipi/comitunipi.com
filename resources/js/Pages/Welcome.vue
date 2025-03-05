3eaba5bd7ab30ca989626c6ff5fcb9f12e44c7ff
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-05 20:19:49 +0700

    chore: adding layouts components and styling ketua and wakil ketua

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,38 +1,35 @@
 <template>
 
   <Head title="Selamat Datang" />
-
-  <Navbar :user="props.user" />
-
-  <div class="">
-    <div class="relative z-0 w-full bg-black py-48 px-5">
-      <div class="relative z-10 text-center text-white mx-auto max-w-xl">
-        <h1 id="text" class="text-4xl md:text-5xl lg:text-7xl mb-4 font-bold italic">
-          Community of Information Technology
-        </h1>
-        <p class="mb-3 text-lg">Semangat Comit, Salam Teknologi!!</p>
-        <a href="/pendaftaran-anggota"
-          class="mt-3 inline-block rounded-lg text-lg px-4 lg:px-8 py-1.5 lg:py-3 bg-blue-600 text-white">Gabung
-          Sekarang</a>
+  <Main :user="props.user">
+    <div class="">
+      <div class="relative z-0 w-full bg-black py-48 px-5">
+        <div class="relative z-10 text-center text-white mx-auto max-w-xl">
+          <h1 id="text" class="text-4xl md:text-5xl lg:text-7xl mb-4 font-bold italic">
+            Community of Information Technology
+          </h1>
+          <p class="mb-3 text-lg">Semangat Comit, Salam Teknologi!!</p>
+          <a href="/pendaftaran-anggota"
+            class="mt-3 inline-block rounded-lg text-lg px-4 lg:px-8 py-1.5 lg:py-3 bg-blue-600 text-white">Gabung
+            Sekarang</a>
+        </div>
+        <img src="https://comit-unipi.vercel.app/_next/image?url=%2Fbanner.png&w=1920&q=75"
+          class="w-full h-full absolute inset-0 object-cover opacity-50">
       </div>
-      <img src="https://comit-unipi.vercel.app/_next/image?url=%2Fbanner.png&w=1920&q=75"
-        class="w-full h-full absolute inset-0 object-cover opacity-50">
+      <About />
+      <Discover />
+      <Mentor />
     </div>
-    <About />
-    <Discover />
-    <Mentor />
-  </div>
-  <Footer />
+  </Main>
+
 </template>
 
 <script setup>
 import About from '@/components/About.vue';
 import Discover from '@/components/Discover.vue';
-import Footer from '@/components/Footer.vue';
 import Mentor from '@/components/Mentor.vue';
-import Navbar from '@/components/Navbar.vue';
+import Main from '@/Layouts/Main.vue';
 import { Head } from '@inertiajs/vue3';
-
 const props = defineProps({
   user: Object,
 });
---
+++ resources/js/components/BannerKepengurusan.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="relative z-0 w-full bg-black py-[200px] px-5">
+    <div class="relative z-10 text-center text-white mx-auto max-w-xl">
+      <p class="text-lg font-bold">Perkenalkan</p>
+      <h1 id="text" class="text-4xl md:text-5xl font-bold italic">
+        Ketua dan Wakil Ketua Comit
+      </h1>
+    </div>
+    <img src="https://comit-unipi.vercel.app/_next/image?url=%2Farduino.png&w=1920&q=75"
+      class="w-full h-full absolute inset-0 object-cover opacity-50">
+  </div>
+</template>
