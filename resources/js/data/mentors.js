cf224042abcd51ac402add7d03e65554afed7c88
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-19 17:55:07 +0700

    chore: create data for mentors

---
+++ resources/js/data/mentors.js
@@ -0,0 +1,175 @@
+export const mentors = [
+  {
+    id: 1,
+    nama: "Idham Mubarok",
+    img: "/images/mentor/mentor_program_1.png",
+    job: "Mentor Programming",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/linkedin.png",
+        link: "https://www.linkedin.com/in/idham-mubarok-71543b171/",
+      },
+      {
+        id: 2,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/idham_mubarok/",
+      },
+      {
+        id: 3,
+        icon: "/images/icons/facebook.png",
+        link: "https://web.facebook.com/idam.mubarok",
+      },
+    ],
+  },
+  {
+    id: 1,
+    nama: "Eko Aulian Saputra",
+    img: "/images/mentor/mentor_program_2.jpeg",
+    job: "Mentor Programming",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/linkedin.png",
+        link: "https://www.linkedin.com/in/eko-aulian-saputra-629400187/",
+      },
+    ],
+  },
+  {
+    id: 3,
+    nama: "Hidayatulloh",
+    img: "/images/mentor/mentor_design_1.jpeg",
+    job: "Mentor Design Grafis",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/mashidayat96?igsh=aXhka3B3bjUzNWRl",
+      },
+    ],
+  },
+  {
+    id: 4,
+    nama: "Aditya Anwary",
+    img: "/images/mentor/mentor_design_2.jpeg",
+    job: "Mentor Design Grafis",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/linkedin.png",
+        link: "https://www.linkedin.com/in/aditya-anwary-graphic-designer-a6486b1b2/",
+      },
+      {
+        id: 2,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/adnr_id/",
+      },
+      {
+        id: 3,
+        icon: "/images/icons/tiktok.png",
+        link: "https://www.tiktok.com/@adnrgrafisidea",
+      },
+    ],
+  },
+  {
+    id: 5,
+    nama: "Vikri Fahruroji",
+    img: "/images/mentor/mentor_design_3.jpeg",
+    job: "Mentor Design Grafis",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/linkedin.png",
+        link: "https://www.linkedin.com/in/vikri-fahruroji-49b703275/",
+      },
+      {
+        id: 2,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/fahrurojivikri/",
+      },
+    ],
+  },
+  {
+    id: 6,
+    nama: "Duta Kusuma Dewa",
+    img: "/images/mentor/mentor_comp_and_network_1.jpeg",
+    job: "Mentor Comp & Network",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/captdeekey/",
+      },
+    ],
+  },
+  {
+    id: 7,
+    nama: "Romi Aji S",
+    img: "/images/mentor/mentor_comp_and_network_2.jpeg",
+    job: "Mentor Comp & Network",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/romiaji14_/",
+      },
+    ],
+  },
+  {
+    id: 8,
+    nama: "M Hussein Hamid",
+    img: "/images/mentor/mentor_comp_and_network_3.jpeg",
+    job: "Mentor Comp & Network",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/linkedin.png",
+        link: "https://www.linkedin.com/in/muhammad-hussein-hamid-026381253/",
+      },
+      {
+        id: 2,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/xen56.co_/",
+      },
+    ],
+  },
+  {
+    id: 8,
+    nama: "Ahmad iif Muhili, S.Kom",
+    img: "/images/mentor/mentor_comp_and_network_4.jpg",
+    job: "Mentor Comp & Network",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/aifmuhili",
+      },
+    ],
+  },
+  {
+    id: 9,
+    nama: "Risna Ayu Lestari",
+    img: "/images/mentor/mentor_ms_office_1.jpeg",
+    job: "Mentor Microsoft Office",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/yumitayo_",
+      },
+    ],
+  },
+  {
+    id: 10,
+    nama: "Yumita Aprina",
+    img: "/images/mentor/mentor_ms_office_2.jpeg",
+    job: "Mentor Microsoft Office",
+    sosmed: [
+      {
+        id: 1,
+        icon: "/images/icons/instagram.png",
+        link: "https://www.instagram.com/yumitayo_",
+      },
+    ],
+  },
+]
