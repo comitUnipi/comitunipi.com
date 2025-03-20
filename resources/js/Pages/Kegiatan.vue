e5d5b61e8c38f39aef350ccb211a3d65d8db2c86
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-20 12:22:25 +0700

    chore: create reusable card for kegiatan

--- resources/js/Pages/Kegiatan.vue
+++ resources/js/Pages/Kegiatan.vue
@@ -13,27 +13,7 @@
     <div class="container mx-auto py-20 px-5">
       <SubHeading title="Kegiatan Kami" description="Kami dengan bangga mempersembahkan berbagai kegiatan dan acara yang telah kami selenggarakan. Setiap acara
             dirancang untuk memberikan pengalaman yang berkesan dan penuh makna." />
-      <article>
-        <div class="flex flex-col md:flex-row items-center md:items-start gap-6 mb-10">
-          <img src="/images/100103.png" alt="kegiatan" class="w-full md:w-1/2 h-auto md:rounded-md shadow-md">
-          <div class="md:max-w-3xl">
-            <h2 class="text-2xl font-bold mb-4">Workshop Data Analyst</h2>
-            <p class="text-md text-justify text-gray-700 mb-4">Dalam Workshop Data Analyst yang telah kami
-              laksanakan, peserta memperoleh keterampilan penting dalam menganalisis dan menginterpretasikan data.</p>
-          </div>
-        </div>
-      </article>
-      <article>
-        <div class="flex flex-col md:flex-row items-center md:items-start gap-6">
-          <img src="/images/100101.png" alt="kegiatan" class="w-full md:w-1/2 h-auto md:rounded-md shadow-md">
-          <div class="md:max-w-3xl">
-            <h2 class="text-2xl font-bold mb-4">Workshop Arduino</h2>
-            <p class="text-md text-justify text-gray-700 mb-4">Workshop Arduino yang telah kami selenggarakan
-              memberikan pengalaman langsung kepada peserta dalam dunia elektronik dan pemrograman. Peserta belajar
-              menggunakan platform Arduino untuk merancang dan mengembangkan project tentang lampu pintar.</p>
-          </div>
-        </div>
-      </article>
+      <Kegiatan />
     </div>
   </Main>
 </template>
@@ -42,6 +22,7 @@
 import Main from '@/Layouts/Main.vue';
 import Loading from '@/components/Loading.vue';
 import Heading from '@/components/ui/Heading.vue';
+import Kegiatan from '@/components/Kegiatan.vue';
 import SubHeading from '@/components/ui/SubHeading.vue';
 
 import { Head } from '@inertiajs/vue3';
---
+++ resources/js/components/Kegiatan.vue
@@ -0,0 +1,30 @@
+<template>
+  <article>
+    <KegiatanCard v-for="item in kegiatan" :img="item.img" :title="item.title" :description="item.description" />
+  </article>
+</template>
+
+<script setup>
+import KegiatanCard from './ui/KegiatanCard.vue';
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      kegiatan: [
+        {
+          img: '/images/100103.png',
+          title: 'Workshop Data Analyst',
+          description: 'Dalam Workshop Data Analyst yang telah kami laksanakan, peserta memperoleh keterampilan penting dalam menganalisis dan menginterpretasikan data.',
+        },
+        {
+          img: '/images/100101.png',
+          title: 'Workshop Arduino',
+          description: 'Workshop Arduino yang telah kami selenggarakan memberikan pengalaman langsung kepada peserta dalam dunia elektronik dan pemrograman. Peserta belajar menggunakan platform Arduino untuk merancang dan mengembangkan project tentang lampu pintar.',
+        },
+      ],
+    };
+  },
+};
+</script>
