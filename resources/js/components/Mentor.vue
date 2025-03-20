307804bd013cb4a4b193c26f9fba0ba2de21e34e
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-20 12:02:42 +0700

    chore: create reusable card for mentor

--- resources/js/components/Mentor.vue
+++ resources/js/components/Mentor.vue
@@ -3,30 +3,8 @@
     <div class="container mx-auto">
       <SubHeading subtitle="Mentor" title="Perkenalkan Mentor Kami"
         description="Kami mempunyai beberapa mentor yang sudah berpengalaman di bidangnya masing-masing." />
-
       <div class="flex flex-wrap justify-center mx-4">
-        <div v-for="mentor in mentors" :key="mentor.id" class="w-full sm:w-1/2 lg:w-1/4 px-4">
-          <div class="mb-10">
-            <div class="relative w-[170px] h-[170px] rounded-full z-10 mx-auto mb-6">
-              <img :src="mentor.img" alt="image" class="w-full rounded-full" />
-            </div>
-            <div class="text-center">
-              <h4 class="font-medium text-xl text-dark mb-2">
-                {{ mentor.nama }}
-              </h4>
-              <p class="font-medium text-sm text-gray-700 mb-5">
-                {{ mentor.job }}
-              </p>
-              <div class="flex items-center justify-center">
-                <div v-for="sosmed in mentor.sosmed" :key="sosmed.id" class="mr-3">
-                  <a :href="sosmed.link" target="_blank">
-                    <img :src="sosmed.icon" alt="icon" class="w-6 h-6 hover:scale-110" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <MentorCard v-for="mentor in mentors" :key="mentor.id" :mentor="mentor" />
       </div>
     </div>
   </section>
@@ -35,4 +13,5 @@
 <script setup>
 import { mentors } from '@/data/mentors';
 import SubHeading from './ui/SubHeading.vue';
+import MentorCard from './ui/MentorCard.vue';
 </script>
