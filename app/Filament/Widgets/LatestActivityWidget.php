fd84a1a44cf2601078806a206b185ee9a1657312
Yuefii <muffidahmadiawan31@gmail.com>
2025-03-21 10:45:00 +0700

    chore: removed some features & migration

--- app/Filament/Widgets/LatestActivityWidget.php
+++ app/Filament/Widgets/LatestActivityWidget.php
@@ -3,21 +3,16 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Activity;
-use App\Models\Event;
 use Carbon\Carbon;
 use Filament\Widgets\Widget;
 
 class LatestActivityWidget extends Widget
 {
     protected static string $view = 'filament.widgets.latest-activity-widget';
-    
+
     public $activityInfo;
     public $activity_date;
     public $activity_location;
-
-    public $event_info;
-    public $event_date;
-    public $event_location;
 
     public function mount()
     {
@@ -33,20 +28,6 @@ class LatestActivityWidget extends Widget
             $this->activityInfo = "Belum ada kegiatan.";
             $this->activity_date = null;
             $this->activity_location = null;
-        }
-
-        $latestEvent = Event::whereDate('event_date', '>=', now())
-            ->orderBy('event_date', 'asc')
-            ->first();
-
-        if ($latestEvent) {
-            $this->event_info = $latestEvent->work_program->name;
-            $this->event_date = Carbon::parse($latestEvent->event_date)->translatedFormat('d F Y');
-            $this->event_location = $latestEvent->location;
-        } else {
-            $this->event_info = "Belum ada event.";
-            $this->event_date = null;
-            $this->event_location = null;
         }
     }
 
