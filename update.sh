28c3f1498eeda30549a6b75bc0128ce61f12b9ea
Yuefii <muffidahmadiawan31@gmail.com>
2025-07-21 21:33:15 +0700

    chore: create auto update

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo "🔄 Reset repo..."
+git reset --hard HEAD
+
+echo "📥 Pull branch main..."
+git pull origin main
+
+echo "📦 Install dependencies PHP (Composer)..."
+composer install --ignore-platform-req=ext-fileinfo
+
+echo "📦 Install dependencies JS (npm)..."
+npm install
+
+echo "🏗️ Build SSR..."
+npm run build:ssr
+
+echo "🧹 Menghapus hasil build sebelumnya di folder tujuan..."
+rm -rf ../favicon ../build ../images ../robots.txt
+
+echo "🚚 Memindahkan hasil build ke folder parent..."
+cd public/
+cp -r build/ ../../
+cp -r favicon/ ../../
+cp -r images/ ../../
+cp -r robots.txt ../../
+
+echo "✅ Update selesai."
