daf58d225ed45aca90fac287485bd769d9cc9123
Yuefii <muffidahmadiawan31@gmail.com>
2025-07-23 08:40:19 +0700

    chore: adding public route for core page

---
+++ routes/public.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    $userCount = User::count();
+
+    return Inertia::render('Index', [
+        'userCount' => $userCount,
+    ]);
+});
+
+Route::get('/visi-dan-misi', function () {
+    return Inertia::render('VisiMisi');
+});
+
+Route::get('/mentor-kami', function () {
+    return Inertia::render('MentorKami');
+});
+
+Route::get('/galeri-kami', function () {
+    return Inertia::render('Galeri');
+});
+
+Route::get('/kegiatan-kami', function () {
+    return Inertia::render('Kegiatan');
+});
+
+Route::prefix('kepengurusan')->group(function () {
+    Route::get('/ketua-dan-wakil-ketua-umum', function () {
+        return Inertia::render('Kepengurusan/KetuaDanWakilKetua');
+    });
+    Route::get('/sekretaris', function () {
+        return Inertia::render('Kepengurusan/Sekretaris');
+    });
+    Route::get('/bendahara', function () {
+        return Inertia::render('Kepengurusan/Bendahara');
+    });
+    Route::get('/sdm', function () {
+        return Inertia::render('Kepengurusan/SDM');
+    });
+    Route::get('/humas-internal', function () {
+        return Inertia::render('Kepengurusan/HumasInternal');
+    });
+    Route::get('/humas-eksternal', function () {
+        return Inertia::render('Kepengurusan/HumasEksternal');
+    });
+    Route::get('/koordinator', function () {
+        return Inertia::render('Kepengurusan/Koordinator');
+    });
+    Route::get('/prasarana', function () {
+        return Inertia::render('Kepengurusan/Prasarana');
+    });
+    Route::get('/kominfo', function () {
+        return Inertia::render('Kepengurusan/Kominfo');
+    });
+    Route::get('/staff-design-grafis', function () {
+        return Inertia::render('Kepengurusan/StaffDesignGrafis');
+    });
+    Route::get('/staff-programming', function () {
+        return Inertia::render('Kepengurusan/StaffProgramming');
+    });
+    Route::get('/staff-comp-and-network', function () {
+        return Inertia::render('Kepengurusan/StaffCompAndNetwork');
+    });
+    Route::get('/staff-microsoft-office', function () {
+        return Inertia::render('Kepengurusan/StaffMicrosoftOffice');
+    });
+});
