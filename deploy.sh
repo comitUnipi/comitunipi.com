3e215cc383b36cbbb66a04fb0be92d0d311d3eb0
Yuefii <muffidahmadiawan31@gmail.com>
2025-08-11 22:54:11 +0700

    chore: change new workflows actions

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,9 @@
 
 set -e
 
-echo "Reset repo..."
+echo "Reset repo & Pull branch main..."
 git reset --hard HEAD
-
-echo "Pull branch main..."
 git pull origin main
-
-echo "Install dependencies PHP (Composer)..."
-composer install --ignore-platform-req=ext-fileinfo --no-dev --optimize-autoloader
-composer dump-autoload --optimize
-
-echo "Clear Laravel cache..."
-php artisan optimize:clear
-php artisan cache:clear
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-
-echo "Rebuild Laravel cache..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
 echo "Install dependencies JS (npm)..."
 npm ci
@@ -30,12 +12,27 @@ npm ci
 echo "Build SSR..."
 npm run build:ssr
 
+echo "Install dependencies PHP (Composer)..."
+composer install --ignore-platform-req=ext-fileinfo --no-dev --optimize-autoloader
+composer dump-autoload --optimize
+
+echo "Clear Laravel cache..."
+php artisan optimize:clear
+
+echo "Rebuild Laravel cache..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Migration database..."
+php artisan migrate --force
+
 echo "Menghapus hasil build sebelumnya di folder tujuan..."
 rm -rf ../favicon ../build ../images ../robots.txt
 
 echo "Memindahkan hasil build ke folder parent..."
 cd public/
-cp -r build/ ../../
+mv build/ ../../
 cp -r favicon/ ../../
 cp -r images/ ../../
 cp -r robots.txt ../../
