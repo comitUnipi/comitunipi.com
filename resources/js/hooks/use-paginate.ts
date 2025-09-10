d3dc4e7b5c1984c7d24e24e5cb579993388e2b75
Yuefii <muffidahmadiawan31@gmail.com>
2025-09-10 22:49:38 +0700

    chore: refact paginate hook(add get filter params)

--- resources/js/hooks/use-paginate.ts
+++ resources/js/hooks/use-paginate.ts
@@ -1,14 +1,14 @@
 import { router } from '@inertiajs/react';
-
 interface Props {
   routeName: string;
+  getFilterParams?: () => Record<string, unknown>;
 }
 
-export default function usePaginate({ routeName }: Props) {
+export default function usePaginate({ routeName, getFilterParams }: Props) {
   const handlePageChange = (page: number) => {
     router.get(
       route(routeName),
-      { page },
+      { page, ...getFilterParams },
       {
         preserveState: true,
         preserveScroll: true,
