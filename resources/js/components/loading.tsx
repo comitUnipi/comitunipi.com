320883832a11cbaa8131f658d17a465dd40e3cf9
Cyou <callmeyuefii@gmail.com>
2025-09-16 16:59:57 +0700

    chore: create loading pages with logo COMIT

---
+++ resources/js/components/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="fixed top-0 left-0 z-[9999] flex h-full w-full items-center justify-center bg-white">
+      <div className="absolute h-[250px] w-[250px] animate-spin rounded-full border-4 border-white/30 border-t-[#3498db]"></div>
+      <img src="/images/logo/logo_black.png" alt=" " className="pointer-events-none absolute z-10 h-[150px] w-[150px]" />
+    </div>
+  );
+}
--- resources/js/layouts/main-layout.tsx
+++ resources/js/layouts/main-layout.tsx
@@ -1,7 +1,14 @@
 import Footer from '@/components/core-footer';
 import Navbar from '@/components/core-navbar';
+import Loading from '@/components/loading';
+import useLoading from '@/hooks/use-loading';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  const { isLoading } = useLoading();
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Navbar />
